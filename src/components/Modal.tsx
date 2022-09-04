227e86a49f0b14eeaa86cc6b31565a83ca750320
agnarrarendelle <61732208+agnarrarendelle@users.noreply.github.com>
2022-09-05 00:11:08 +0800

    added some comments

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -3,26 +3,24 @@ import "./Modal.css";
 import SubTaskList from "./SubTaskList";
 
 interface Props {
+  //a function used to change whether to open the modal or not. it will be triggered when users click on close button in the modal
   setOpenModal: (mode: boolean) => void;
   title: string;
   id: string;
+  //a function triggered when users change task title inside the modal
   changeTodos: (newTitle: string, id: string) => void;
+  //a todo object's subTasks, it wil be displayed in the modal
   subTasks: string[];
+  //a function used to add more subtasks to a todo object
   addSubTask: (taskName: string, id: string) => void;
 }
 
-// const Modal=(props:Props)=>{
-//     return <div>I am modal  title:{props.title}
-//         <button onClick={()=>{props.setOpenModal(false)}}></button>
-//     </div>
-// }
+
 
 const Modal = (props: Props) => {
+  
+  //create a ref to the user input so that we can get what the users have inputed 
   const newTitle = useRef<HTMLInputElement>(null);
-  console.log(newTitle.current);
-  // useEffect(()=>{
-
-  // })
 
   return (
     <div className="modalBackground">
@@ -61,6 +59,9 @@ const Modal = (props: Props) => {
           </button>
           <button
             onClick={() => {
+              //when clicking on confirm button, it will first check whether users have given the task a new name
+              //and if so, call changeTodos with the todo object's id to change the task's title
+              //After that, call setOpenModal with false to close the modaal
               let enteredName = newTitle.current!.value.trim();
               let name = enteredName === "" ? props.title : enteredName;
 
