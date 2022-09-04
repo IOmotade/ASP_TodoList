28661f3521652a77167df3f2fa17df98d8d79a85
agnarrarendelle <61732208+agnarrarendelle@users.noreply.github.com>
2022-09-05 00:26:09 +0800

    added some comments

--- src/components/SubTaskList.tsx
+++ src/components/SubTaskList.tsx
@@ -1,25 +1,33 @@
 import { useState } from "react";
 import "./SubTaskList.css"
 interface Props {
+  //subTaks array that will be displayed in the modal
   subTasks: string[];
+  //the id of a todo object
   id: string;
+  //a function to added more subtasks to a todo object
   addSubTasks: (taskName: string, id: string) => void;
 }
 
 const SubTaskList = (props: Props) => {
+  //the new subtask name the users have entered
   const [enteredTask, setEnteredTask] = useState("");
 
+  //triggered when users enter anything in the subtask input
+  //it will convert the input to lowercase then update enteredTask with the input
   const onTaskNameChange = (e: React.FormEvent<HTMLInputElement>) => {
     const input = e.currentTarget.value.trim().toLowerCase();
-    console.log(input);
     setEnteredTask(input);
   };
 
+  //triggered when users click on "add" button to added new subtask
+  //it will call addSubTasks with the task name and the todo object id to update the todo object's subtasks
   const onButtonClicked = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     props.addSubTasks(enteredTask, props.id);
   };
 
+  //return all suntasks
   const getSubTasks = () => {
     const tasks = props.subTasks.map((task) => {
       return <li key={task}>{task}</li>;
