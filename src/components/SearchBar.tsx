c1a55561fee1e27ec4b1564a3493eb84023a6a25
agnarrarendelle <61732208+agnarrarendelle@users.noreply.github.com>
2022-09-05 00:17:53 +0800

    added some comments

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,13 +1,16 @@
 import "./SearchBar.css"
 
 interface Prop{
+    //a function to set user input in the search bar
     setUserInput: (input:string)=>void
 }
 
 const SearchBar = (props:Prop) => {
+    //triggered when user enters anything in the search bar
+    //it will get the new input, convert it to lowercase
+    //then set the userInput in App to the new input
     const onSearchBarChange = (e:React.FormEvent<HTMLInputElement> )=>{
         const input = e.currentTarget.value.trim().toLowerCase()
-        console.log(input)
         props.setUserInput(input)
     }
 
